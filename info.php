451984ff0eeeb82c29cab3ed25c734210babe14f
Gihan Chathuranga Gunathilaka <141658614+Chathuwa-67@users.noreply.github.com>
2023-10-23 04:39:20 +0530

    Insert Data Into Separate Tables
    Using foreign keys I connected tables. when the user clicks the submit button data will be sent to respected tables.

--- info.php
+++ info.php
@@ -1,54 +1,123 @@
 <?php
-// Establish a connection to your MySQL database. 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "drone_registration";
+// Database connection details
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'civil_aviation_authority';
 
-$conn = new mysqli($servername, $username, $password, $database);
+// Create a database connection
+$conn = mysqli_connect($host, $username, $password, $database);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Check the connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 
-// Get data from the form
-$nic = $_POST['nic'];
+
+// Get data from the form for Table 1 Pilot details
+$pilot_id = $_POST['pilot_id'];
 $name = $_POST['name'];
 $address = $_POST['address'];
-$insurance = $_POST['insurance'];
-$manufacture = $_POST['manufacture'];
-$model = $_POST['model'];
-$serial = $_POST['serial'];
-$altitude = $_POST['altitude'];
-$drone_class = $_POST['drone_class'];
-$type = $_POST['type'];
+$license = $_POST['license'];
 
+// Insert data into Table 1
+$sql_table1 = "INSERT INTO pilot_details (pilot_id,name,address,license) VALUES ('$pilot_id', '$name', '$address', '$license')";
 
-
-
-//Check Duplicate primary key
-$checkQuery = "SELECT * FROM pilot_details WHERE nic = '$nic'";
-$result = $conn->query($checkQuery);
-
-if ($result->num_rows > 0) {
-    
-    echo "Error: This NIC is already in use.";
-} 
-
-// Insert the data into the database
-else{
-
-    $sql = "INSERT INTO pilot_details (nic,name,address,insurance,manufacture,model,serial,altitude,drone_class,type) VALUES ('$nic','$name','$address','$insurance','$manufacture','$model','$serial','$altitude','$drone_class','$type')";
-
-    if ($conn->query($sql) === TRUE) {
-        header('location:thanks.html');
-       // echo "Data inserted successfully!";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-
+if (mysqli_query($conn, $sql_table1)) {
+    echo "Data inserted into Table 1 successfully.<br>";
+} else {
+    echo "Error: " . mysqli_error($conn);
 }
 
 
-$conn->close();
+// Get data from the form for Table 2 Drone Model Details
+$drone_model_id = $_POST['drone_model_id'];
+$model_name = $_POST['model_name'];
+
+
+// Insert data into Table 2
+$sql_table2 = "INSERT INTO drone_models (drone_model_id,model_name) VALUES ('$drone_model_id','$model_name')";
+
+if (mysqli_query($conn, $sql_table2)) {
+    echo "Data inserted into Table 2 successfully.<br>";
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+
+// Get data from the form for Table 3 Drone Details
+$drone_id = $_POST['drone_id'];
+$serial = $_POST['serial'];
+
+
+// Insert data into Table 3
+$sql_table3 = "INSERT INTO drone_details (drone_id,serial,drone_model_id,pilot_id) VALUES ('$drone_id', '$serial','$drone_model_id','$pilot_id')";
+
+if (mysqli_query($conn, $sql_table3)) {
+    echo "Data inserted into Table 3 successfully.<br>";
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+
+
+// Insert data into Table 4 Insuarence Details
+$insurance_id = $_POST['insurance_id'];
+$period = $_POST['period'];
+$provider = $_POST['provider'];
+
+$sql_table4 = "INSERT INTO insurence_details (insurance_id,period,provider,drone_id) VALUES ('$insurance_id', '$period','$provider','$drone_id')";
+
+if (mysqli_query($conn, $sql_table4)) {
+    echo "Data inserted into Table 4 successfully.<br>";
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+
+
+// Insert data into Table 5 Insuarence Details
+$company_id = $_POST['company_id'];
+$company_name = $_POST['company_name'];
+$manufacture_date = $_POST['manufacture_date'];
+
+$sql_table5 = "INSERT INTO company_details (company_id,company_name,manufacture_date,drone_model_id) VALUES ('$company_id', '$company_name','$manufacture_date','$drone_model_id')";
+
+if (mysqli_query($conn, $sql_table5)) {
+    echo "Data inserted into Table 5 successfully.<br>";
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+
+
+// Insert data into Table 6 Insuarence Details
+$specification_id = $_POST['specification_id'];
+$drone_class = $_POST['drone_class'];
+$drone_type = $_POST['drone_type'];
+
+$sql_table6 = "INSERT INTO specification_details (specification_id,drone_class,drone_type,drone_model_id) VALUES ('$specification_id', '$drone_class','$drone_type','$drone_model_id')";
+
+if (mysqli_query($conn, $sql_table6)) {
+    echo "Data inserted into Table 6 successfully.<br>";
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+
+
+//Insert into full Details
+$sql_table7 = "INSERT INTO full_details (pilot_id,drone_id,drone_model_id,specification_id,company_id,insurance_id ) VALUES ('$pilot_id', '$drone_id','$drone_model_id','$specification_id','$company_id','$insurance_id')";
+
+if (mysqli_query($conn, $sql_table7)) {
+    echo "Data inserted into Table 7 successfully.<br>";
+    header("Location:thanks.php ");
+exit;
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
+ 
+
+
+
+
+
+
+// Close the database connection
+mysqli_close($conn);
 ?>
